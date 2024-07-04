7775805e22130c54bc283ee88320af3d1171db58
Camilocodecali <camilorodriguezco7@gmail.com>
2024-07-04 02:40:47 -0500

    Update Cambio de estilos en fondo y filtros

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -17,10 +17,10 @@ export default function BudgetTracker() {
         <CircularProgressbar
             value={percentage}
             styles={buildStyles({
-              pathColor: percentage === 100 ? '#dc2626' : '#3b82f6',
+              pathColor: percentage === 100 ? '#dc2626' : '#3730A3',
               trailColor: '#f5f5f5',
               textSize: 8,
-              textColor: '#3b82f6'
+              textColor: '#3730A3'
 
             })}
             text={`${percentage}% Gastado`}
@@ -30,7 +30,7 @@ export default function BudgetTracker() {
         <button
             type="button"
             className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
-            onClick={() => dispatch({type:'reset-budget', payload:{budget: state.budget}})}
+            onClick={() => dispatch({type:'reset-budget'})}
         >
             Resetear tu presupuesto
         </button>
--- src/components/FilterByCategory.tsx
+++ src/components/FilterByCategory.tsx
@@ -12,10 +12,10 @@ export default function FilterByCategory() {
     }
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-10">
+    <div className="bg-indigo-400 shadow-lg rounded-lg p-10">
       <form >
         <div className="flex flex-col md:flex-row md:items-center gap-5">
-            <label htmlFor="category">Filtrar Gastos</label>
+            <label htmlFor="category" className="text-white text-bold text-2xl">Filtrar Gastos</label>
             <select 
                 id="category"
                 className="bg-slate-100 p-3 flex-1 rounded"
